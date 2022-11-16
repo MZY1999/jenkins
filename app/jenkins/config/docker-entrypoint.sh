ac210b8f3274d39bd3ba280e48327a8988f8386b
Jesse <jesse@incontek.com>
2022-11-16 16:51:20 +0800

    fix bug

--- app/jenkins/config/docker-entrypoint.sh
+++ app/jenkins/config/docker-entrypoint.sh
@@ -2,6 +2,5 @@
 # Info:Jenkins 入口启动脚本
 # Date:2022.11.10
 
-JAVA_OPTS="-Djava.awt.headless=true -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true"
 cd /usr/local/jenkins/
-java ${JAVA_OPTS} -jar jenkins.war --httpPort=8080
+java  -jar jenkins.war --httpPort=8080
